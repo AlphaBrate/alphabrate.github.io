0646984e1acbc58447d5b7e56bc754d4b941645a
AlphaBrate <87927890+AlphaBrate@users.noreply.github.com>
2024-09-02 16:23:16 +0800

    Supplemental Updates

--- articles/content.js
+++ articles/content.js
@@ -93,9 +93,13 @@ fetch("/articles/articles.json").then(res => res.json()).then(articles => {
 
             el.innerHTML += `<span class="copy-here"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9 15H5C3.89543 15 3 14.1046 3 13V5C3 3.89543 3.89543 3 5 3H13C14.1046 3 15 3.89543 15 5V9M11 21H19C20.1046 21 21 20.1046 21 19V11C21 9.89543 20.1046 9 19 9H11C9.89543 9 9 9.89543 9 11V19C9 20.1046 9.89543 21 11 21Z" stroke="#1F1F1F" stroke-width="1.5" /></svg></span>`;
 
-            el.querySelector(".copy-here").addEventListener("click", () => {
-                el = el.querySelector(".copy-here");
-                let code = el.parentElement.querySelector("code").innerText;
+            el.querySelector(".copy-here").addEventListener("click", (element) => {
+                element = element.target;
+                while (element.tagName != "PRE") {
+                    element = element.parentElement;
+                }
+                element = element.querySelector(".copy-here");
+                let code = element.parentElement.querySelector("code").innerText;
 
                 navigator.clipboard.writeText(code).then(() => {
                     pujs.alert("Copied to clipboard", "success");
